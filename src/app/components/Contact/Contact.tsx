8af8fb19fcaea98dc07d82a975ded10f4b0420a1
Guille <guipradi5@gmail.com>
2023-12-21 13:02:46 +0100

    Contact window

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,14 +1,18 @@
 "use client"
 import React from "react";
+import './Contact.css'
 
 
 export default function Contact() {
     return (
-    <div>
-        <div>Logo de correo en buzon<a href="">Email</a></div>
-        <div>Logo linkedin pixelad<a href="">Linkedin</a></div>
-        <div>Logo github pixelado<a href="">Github</a></div>
-        <div>Logo twitter pixelado<a href="">Twitter</a></div>
+    <div className="contacts">
+        <div className="contacts__text">You can contact me through the following channels:</div>
+        <ul>
+            <li><a href="mailto:guipradi5@gmail.com"><img src="./icons/email.png" />Write me an e-mail</a></li>
+            <li><a href="https://www.linkedin.com/in/guillermo-pradas-di-liscia-92178210a/" target="_blank"><img src="./icons/linkedin.png" />Linkedin page</a></li>
+            <li><a href="https://github.com/guipradi5" target="_blank"><img src="./icons/github.png" />Github account</a></li>
+            <li><a href="https://twitter.com/gillthedev" target="_blank"><img src="./icons/twitter.png" />Twitter page</a></li>
+        </ul>
     </div>
     )
 }
