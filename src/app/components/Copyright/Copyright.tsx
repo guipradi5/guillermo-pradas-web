c9b94cfc0f0fb962d349ae8fd85049778fbe3a94
Guille <guipradi5@gmail.com>
2023-12-21 15:28:06 +0100

    Add font credit for Peter Hull

--- src/app/components/Copyright/Copyright.tsx
+++ src/app/components/Copyright/Copyright.tsx
@@ -4,6 +4,12 @@ import React, {useRef} from 'react';
 const year = new Date().getFullYear()
 export default function CopyRight () {
   return (
-    <p>&copy;{year} - Guillermo Pradas</p>
+    <div>
+      <p>
+        &copy;{year} - Guillermo Pradas
+        <br /><br />
+        Font: VT323 by <a href="https://github.com/phoikoi" target="_blank">Peter Hull</a>
+      </p>
+    </div>
         )
 }
