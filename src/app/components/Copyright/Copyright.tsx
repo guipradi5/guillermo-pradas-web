f5403d7cc97907c0f9e8e0d7ec12c33c11eaf382
Guille <guipradi5@gmail.com>
2023-12-21 15:54:25 +0100

    Dedicatoria

--- src/app/components/Copyright/Copyright.tsx
+++ src/app/components/Copyright/Copyright.tsx
@@ -6,11 +6,13 @@ export default function CopyRight () {
   return (
     <div>
       <p>
-        Font: VT323 by <a href="https://github.com/phoikoi" target="_blank">Peter Hull</a>
+        Font: VT323 by <a href="https://github.com/phoikoi" target="_blank">Peter Hull</a>.
         <br /><br />
-        <a href="https://github.com/guipradi5/guillermo-pradas-web" target="_blank">This website's source code</a> @ Github
+        <a href="https://github.com/guipradi5/guillermo-pradas-web" target="_blank">This website's source code</a> @ Github.
+        <br /><br />
+        &#10084; Yess // Zula & Aza &#10084;
         <br />
-        &copy;{year} - Guillermo Pradas
+        &copy;{year} - Guillermo Pradas.
       </p>
     </div>
         )
