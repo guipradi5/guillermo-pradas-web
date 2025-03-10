a8fd6629a5cecd712bb43b2240659bde823a50a4
Guille <guipradi5@gmail.com>
2025-03-10 15:45:49 +0100

    Pruebas

--- src/app/(admin)/admin/new-post/page.tsx
+++ src/app/(admin)/admin/new-post/page.tsx
@@ -4,13 +4,17 @@ import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import Quill from 'quill';
 import 'react-quill/dist/quill.snow.css'; // Import Quill styles
-
+import {post, put, get} from '../../../../js/api'
 
 const QuillEditor = dynamic(() => import('react-quill'), { ssr: false });
 
 
 export default function Next() {
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
   const [content, setContent] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
+  const [isPublishing, setIsPublishing] = useState(false);
   
   const quillModules = {
     toolbar: [
@@ -43,16 +47,35 @@ export default function Next() {
     'color',
     'code-block',
   ];
-
-
+  
   const handleEditorChange = (newContent:string) => {
-    console.log(newContent);
     setContent(newContent);
   };
+
+  function savePost() {
+    post('https://guillermopradas.com/api/v1/blog-posts', { title, subtitle, body: content, published: false })
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+  }
+
+  function publishPost() {
+
+  }
   
   return (
     <div>
         <div className="h-full w-[90vw]">
+          <h2>Title</h2>
+          <input className='w-[90vw]' id="postTitle" 
+            value={title}
+            onChange={(event) => setTitle(event.target.value)} 
+            type='text'></input><br/>
+          <h2>Subtitle</h2>
+          <input className='w-[90vw]' id="postSubtitle" 
+            value={subtitle}
+            onChange={(event) => setSubtitle(event.target.value)} 
+            type='text'></input>
           <QuillEditor
             value={content}
             onChange={handleEditorChange}
@@ -61,9 +84,12 @@ export default function Next() {
             className="w-full h-[70%] mt-10 bg-white"
           />
         </div>
+        <div>{title}</div>
+        <div>{subtitle}</div>
+        <div>{content}</div>
         <div className="button-wrapper">
-          <button>Save</button>
-          <button>Publish</button>
+          <button onClick={savePost}>Save</button>
+          <button onClick={publishPost}>Publish</button>
         </div>
     </div>
   )
--- src/app/(admin)/admin/posts/page.tsx
+++ src/app/(admin)/admin/posts/page.tsx
@@ -1,7 +1,14 @@
 
 "use client"
+import { get } from "@/js/api";
 
 export default function Next() {
+  
+  get('https://guillermopradas.com/api/v1/blog-posts')
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
+
   return (
     <div>
       <p>Posts</p>
