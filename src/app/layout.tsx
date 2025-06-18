426e6983dc8a2a0d8ad6bd5ffbe1c7a87dac681d
Guille <guipradi5@gmail.com>
2025-06-18 21:55:24 +0200

    Add OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,30 @@ import Home from './Home'
 export const metadata: Metadata = {
   title: "Guillermo Pradas | Web Developer & Game Designer",
   description: "Guillermo Pradas' website. Web Developer & Game Designer",
+  openGraph: {
+    title: "Guillermo Pradas | Web Developer & Game Designer",
+    description: "Guillermo Pradas' website. Web Developer & Game Designer",
+    url: "https://guillermopradas.com/",
+    siteName: "Guillermo Pradas",
+    images: [
+      {
+        url: "https://guillermopradas.com/img/web-preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Guillermo Pradas - Web Developer & Game Designer",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Guillermo Pradas | Web Developer & Game Designer",
+    description: "Guillermo Pradas' website. Web Developer & Game Designer",
+    images: ["https://guillermopradas.com/images/web-preview.png"],
+  },
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <Home>
       {children}
